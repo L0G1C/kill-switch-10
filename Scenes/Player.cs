7463a59532580fb446dcfc5fe5281c8c04a9f52a
L0G1C <contact.leojerez@gmail.com>
2022-10-01 01:08:10 -0400

    Main Menu and Player

---
+++ Scenes/Player.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+ private Vector2 velocity = Vector2.Zero;	
+	 const float ACCELERATION = 450;
+	 const float MAX_SPEED = 100;
+	 const float FRICTION = 450;
+
+	public override void _Ready()
+	{		
+		GD.Print("Ready!");
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		var inputVector = Vector2.Zero;
+		inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+		inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+
+		inputVector = inputVector.Normalized(); //trim vector to normalized length
+
+		if (inputVector != Vector2.Zero)		
+			velocity = velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);			
+		else
+			velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);		
+
+		MoveAndCollide(velocity * delta);
+    }
+}
