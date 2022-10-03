43e3a514aaab13948a0bac4e17d6d8eb2fb6d487
L0G1C <contact.leojerez@gmail.com>
2022-10-03 11:27:44 -0400

    Basic Timer Label

---
+++ Scenes/TimerLabel.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class TimerLabel : Label
+{
+    private Timer _levelTimer;
+    private Timer _waitTimer;
+    private bool _levelTimerStarted;
+    
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _levelTimer = GetNode<Timer>("Timer");
+        _waitTimer = new Timer();
+        _waitTimer.WaitTime = 1;
+        _waitTimer.OneShot = true;
+        AddChild(_waitTimer);
+        _waitTimer.Start();
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+ public override void _Process(float delta)
+ {    
+    if(_waitTimer.TimeLeft == 0 && !_levelTimerStarted)
+    {
+        _levelTimer.Start();
+        _levelTimerStarted = true;
+    }
+    
+    Text =_levelTimer.TimeLeft.ToString();
+ }
+}
