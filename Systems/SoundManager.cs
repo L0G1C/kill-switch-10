9c2cfcecf8b0cb27ed469bfbe7490b92cd3d02e9
L0G1C <contact.leojerez@gmail.com>
2022-10-14 00:20:53 -0400

    SoundManager Scene Autoload with AudioPlayer2D

--- Systems/SoundManager.cs
+++ Systems/SoundManager.cs
@@ -3,13 +3,14 @@ using System;
 
 public class SoundManager : Node
 {
-    private 
+    private AudioStreamPlayer2D _AudioStreamPlayer;
 
     public SoundManager Instance;
 
     public override void _Ready()
     {
         Instance = this;    
+        _AudioStreamPlayer = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
     }
 
     public void HandleStepEvent()
